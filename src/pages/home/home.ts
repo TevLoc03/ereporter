f3d9eddba504663d05721546beb5ac260e619c6d
dominique gaffory <dominique.gaffory@live.fr>
2018-01-15 18:30:24 +0100

    connrx

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { LoginPage } from '../login/login';
+import { InscriptionPage } from '../inscription/inscription';
 
 @Component({
   selector: 'page-home',
@@ -10,5 +12,12 @@ export class HomePage {
   constructor(public navCtrl: NavController) {
 
   }
+  connexion(){
+    this.navCtrl.push(LoginPage);
+  }
 
+
+  inscription(){
+    this.navCtrl.push(InscriptionPage);
+  }
 }
---
+++ src/pages/login/login.ts
@@ -0,0 +1,54 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
+
+/**
+ * Generated class for the LoginPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-login',
+  templateUrl: 'login.html',
+})
+export class LoginPage {  
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private alertCtrl: AlertController) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad LoginPage');
+  }
+
+  forgotPwd() {
+    console.log('ionViewDidLoad LoginPage');
+  }
+
+  presentPrompt() {
+    let alert = this.alertCtrl.create({
+      title: 'Enter your Email',
+      inputs: [
+        {
+          name: 'email',
+          placeholder: 'Email'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Send',
+        }
+      ]
+    });
+    alert.present();
+  }
+}
